e181fb2cc5b139623196b47d4a0e0cbf5b8a0b6c
Muhammad Azri <muhd.azri@fiction-labs.com>
2018-05-13 17:23:37 +0800

    ajax request with axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,18 @@
+import axios from 'axios';
+
 const API_KEY= '2503b57d71199965d4ff1aa7f3f0a46e';
+
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    //request url
+    const url = `${ROOT_URL}&q=${city},my`;
+    const request = axios.get(url);
+
+    return{
+        type: FETCH_WEATHER,
+        payload: request
+    };
+} 

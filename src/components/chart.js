c4a76f0b7004d9f3ecfc844c9cc7ad7da270b61e
Muhammad Azri <lenovorosak@gmail.com>
2018-05-14 08:14:39 +0800

    integrate with google maps

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import React from 'react';
-import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine, SparklinesSpots, SparklinesNormalBand } from 'react-sparklines';
 
 function average(data){
     return _.round(_.sum(data)/data.length);
@@ -11,6 +11,7 @@ export default (props) =>  {
         <div>
             <Sparklines height={120} width={120} data={props.data}>
                 <SparklinesLine color={props.color} />
+                <SparklinesSpots />
                 <SparklinesReferenceLine type="avg" />
             </Sparklines>
             <div>{average(props.data)} {props.units}</div>
--- src/container/weather_list.js
+++ src/container/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
     renderWeather(cityData){
@@ -8,11 +9,11 @@ class WeatherList extends Component {
         const temps = cityData.list.map(weather => weather.main.temp);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
-        // console.log(temps,pressures,humidities );
+        const { lon, lat } = cityData.city.coord;
 
         return(
             <tr key={name}>
-                <td>{name}</td>
+                <td><GoogleMap lon={lon} lat={lat} /></td>
                 <td><Chart data={temps} color="red" units="K" /></td>
                 <td><Chart data={pressures} color="blue" units="hPa" /></td>
                 <td><Chart data={humidities} color="orange" units="%" /></td>
@@ -27,7 +28,7 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperatur (k)</th>
+                        <th>Temperature (k)</th>
                         <th>Pressure (hPa)</th>
                         <th>Humidity (%)</th>
                     </tr>
